d74af13e497fbddc930646433798d5613af01c61
tesabel <tesabel48@naver.com>
2025-05-07 20:53:26 +0900

    [fix]use .env file

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,28 +1,26 @@
 import axios from "axios";
 import qs from "qs";
 
-const API_BASE_URL = 'http://localhost:8000';
-
 export const register = async (userData) => {
-    return axios.post(
-        `${API_BASE_URL}/api/auth/register`,
-        userData,
-        {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
-    );
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/api/auth/register`,
+    userData,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
 
 export const login = async (userData) => {
-    return axios.post(
-        `${API_BASE_URL}/api/auth/login`,
-        qs.stringify(userData),
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        }
-    );
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/api/auth/login`,
+    qs.stringify(userData),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
 };
