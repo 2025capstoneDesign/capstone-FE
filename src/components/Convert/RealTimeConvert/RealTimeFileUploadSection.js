e5e868128bb093122feaf9e836d8a27872f0ab2f
tesabel <tesabel48@naver.com>
2025-06-09 17:59:29 +0900

    fix/css

--- src/components/Convert/RealTimeConvert/RealTimeFileUploadSection.js
+++ src/components/Convert/RealTimeConvert/RealTimeFileUploadSection.js
@@ -132,7 +132,7 @@ function RealTimeFileUploadSection({
         </div>
 
         {/* 실시간 변환 버튼 */}
-        <div className="flex justify-end mb-4 relative -top-10">
+        <div className="flex justify-end mb-4 relative -top-14">
           <button
             className="bg-[#5B7F7C] text-white font-semibold py-2 px-4 rounded-lg"
             onClick={handleConvert}
