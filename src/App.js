bc2aa2fc557685863972e639f9839da0c044f558
woochan0408 <woochan0408@naver.com>
2026-06-10 13:32:52 +0900

    chore: remove dead code (unused components, css, images)
    - Footer.js 삭제 (미사용, App.js 주석 참조만 존재)
    - ResultPage.css, Convert.css, styles/ 삭제 (import 0건)
    - 미사용 이미지 9개 삭제

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ function App() {
                   </Routes>
                 </div>
               </main>
-              {/* <Footer /> */}
             </div>
           </Router>
         </HistoryProvider>
